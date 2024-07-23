3f1d64dad2ad4c93f8bec04ae4e47ebf92d7a7c6
Dave Hurley <djmhur@gmail.com>
2024-07-23 23:47:48 +0100

    pass 8th stage

--- app/main.py
+++ app/main.py
@@ -116,7 +116,7 @@ def get_peer_list(tracker_url,info_hash,file_len):
     sk.connect((host,port))
     urlenc_hash = url_encode(int(info_hash,16).to_bytes(20))
     print("TYPE:",type(urlenc_hash), type(path) )
-    msg = ("GET " + path + ""
+    msg = ("GET " + str(path) + ""
            "?info_hash=" + urlenc_hash + "&peer_id=84922341765498374098"
            "&port=6881"
            "&uploaded=0"
