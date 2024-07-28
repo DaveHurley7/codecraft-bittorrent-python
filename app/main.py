c4afb09c72d04e2adf827c6ef131e496bf9ac70c
Dave Hurley <djmhur@gmail.com>
2024-07-28 05:28:11 +0100

    pass 10th stage

--- app/main.py
+++ app/main.py
@@ -171,7 +171,7 @@ def last_block(block_num,n_blocks,last_size):
 def handle_peer_msgs(peer_sk, piece_id, piecelen):
     print("Waiting")
     while msg := read_msg(peer_sk):
-        print(msg)
+        print(msg,MsgId.Bitfield,msg[0])
         if msg[0] == MsgId.Bitfield:
             print("Have bitfield")
             break
